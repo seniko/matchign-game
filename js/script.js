9f4ec10e6e151b75dd8eb371033c9a1c782a54b5
seniko <sergeykono@gmail.com>
2016-09-19 00:00:47 +0300

    Next level functionality

--- js/script.js
+++ js/script.js
@@ -2,18 +2,57 @@
  * Created by Пользователь on 16.09.2016.
  */
 var numberOfFaces = 5;
-var leftSide = document.getElementById("leftSide");
+var count = 0;
+var theLeftSide = document.getElementById("leftSide");
+var theRightSide = document.getElementById("rightSide");
+var theBody = document.getElementsByTagName("body")[0];
+
 
 function generateFaces() {
-
     for (var i = 0; i < numberOfFaces; i++){
         var image = document.createElement("img");
-        image.setAttribute("src", "img/smile.png");
-        var top = Math.floor(Math.random() * 400);
-        var left = Math.floor(Math.random() * 400);
+        image.setAttribute("src", "img/Awesome.svg");
+        image.setAttribute("width", "100px");
+        var top = Math.floor(Math.random() * 395);
+        var left = Math.floor(Math.random() * 395);
         image.style.top = top + "px";
         image.style.left = left + "px";
-        leftSide.appendChild(image);
+        theLeftSide.appendChild(image);
     }
+    theLeftSide.lastChild.onclick = function nextLevel(event) {
+        event.stopPropagation();
+        while (theLeftSide.firstChild){
+            theLeftSide.removeChild(theLeftSide.firstChild);
+        }
+        while (theRightSide.firstChild){
+            theRightSide.removeChild(theRightSide.firstChild);
+        }
+        numberOfFaces += 5;
+        count += 10;
+        generateFaces();
+        cloneLeftSide();
+    };
 }
 generateFaces();
+
+
+function cloneLeftSide() {
+    var leftSideImages = theLeftSide.cloneNode(true);
+    leftSideImages.removeChild(leftSideImages.lastChild);
+//тут можно указать какой элемент удалить.
+//В данном случае удаляется последний элемент родителя. Однако, можно уазать так:
+//leftSideImages.childNodes[3]
+//где 3 - третий элемент родителя. Соответственно, для усложнения игры, данное число можно установить
+//рандомно => лишний smile.png может быть не только на переднем плане, но и находится ниже или выше
+// по уровню относительно других smile.png. Однако тогда инкремент следует уменьшить, т.к. поле быстро
+// заполняется с каждым уровнем.
+    theRightSide.appendChild(leftSideImages);
+}
+cloneLeftSide();
+
+
+theBody.onclick = function gameOver() {
+    alert("Game over\n" + "You score: " + count);
+    theBody.onclick = null;
+    theLeftSide.lastChild.onclick = null;
+};
